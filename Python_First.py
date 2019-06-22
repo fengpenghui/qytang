82e821e483b8742e09129581b4863590f42d128e
fengph <jcyhdream@163.com>
2019-06-23 02:17:47 +0800

    test1

---
+++ Python_First.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+# coding: utf-8
+# github: https://github.com/fengpenghui
+# 码云: https://gitee.com/fengpenghui0923
+
+result=1+2
+print("计算1 + 2的结果")
+print("计算结果为:",result)
