ade9e9a81c715ce665ff0a70f4a46c8a5016e8cc
fengph <jcyhdream@163.com>
2019-06-23 23:17:49 +0800

    test1

--- task_two.py
+++ task_two.py
@@ -6,12 +6,11 @@
 
 import re
 
-st1 = 'Port-channel1.189       192.168.189.254     YES   CONFIG    up'
+str1 = 'Port-channel1.189       192.168.189.254     YES   CONFIG    up'
 
-result = re.match('\s*(.*\w\d)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(\w*)\s+(\w*)\s+(\w*)\s+(\w*)\s*',st1).groups()
+result = re.match('\s*(.*\w\d)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(\w*)\s+(\w*)\s+(\w*)\s+(\w*)\s*',str1).groups()
 
 print('-'*100)
 print('接口：%-30s'%(result[0]))
 print('IP地址：%-30s'%(result[1]))
 print('状态：%-30s'%(result[-1]))
-
