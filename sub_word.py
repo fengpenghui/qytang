85672021d5c3651675237fa96e64defcd9ed347e
fengph <jcyhdream@163.com>
2019-06-23 13:18:36 +0800

    test1

---
+++ sub_word.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# coding: utf-8
+# github: https://github.com/fengpenghui
+# 码云: https://gitee.com/fengpenghui0923
+
+
+word="scallywag"
+sub_word=word[2:-3]
+print(sub_word)
+
+
+word="scallywag"
+sub_word=word[2:6]
+print(sub_word)
+
+
