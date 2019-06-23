6101b09541f5c58f564b45030ffbfbdbfe671043
fengph <jcyhdream@163.com>
2019-06-23 11:53:12 +0800

    test1

---
+++ Random_IPv4.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# coding: utf-8
+# github: https://github.com/fengpenghui
+# 码云: https://gitee.com/fengpenghui0923
+
+#! /usr/bin/env python3
+
+import random
+
+print (str(random.randrange(1,254)) + '.' +  str(random.randrange(0,255))+ '.' +  str(random.randrange(0,255))+ '.' +  str(random.randrange(0,255)))
+
